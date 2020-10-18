fdfb2740dd8cb8411da482516b49cfa231de370f
李元彪 <274008566@qq.com>
2020-10-18 17:12:01 +0800

    10.18

---
+++ build/precache-manifest.adfd1ad809f00944dbd6a5b28efd423b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c710c28fa516b8f79895",
+    "url": "/static/css/main.c675c59e.chunk.css"
+  },
+  {
+    "revision": "c710c28fa516b8f79895",
+    "url": "/static/js/main.51517505.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "870dec1a91f02cdbe012",
+    "url": "/static/js/2.394f473e.chunk.js"
+  },
+  {
+    "revision": "d525b000ee17d74fa0acfd89d95972be",
+    "url": "/index.html"
+  }
+];
